9c4399a7c750ce5fc26129c82b4efe4562185d41
Lama <lama72316@gmail.com>
2026-05-20 15:14:45 +0000

    Fix simple_shell 0.1

--- hsh.c
+++ hsh.c
@@ -4,51 +4,80 @@
 #include <sys/wait.h>
 
 /**
+ * is_empty - checks if input is only spaces/tabs/newlines
+ * @str: input string
+ *
+ * Return: 1 if empty, 0 otherwise
+ */
+int is_empty(char *str)
+{
+        int i = 0;
+
+        if (!str)
+                return (1);
+
+        while (str[i])
+        {
+                if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+                        return (0);
+                i++;
+        }
+        return (1);
+}
+
+/**
  * main - simple shell
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	pid_t pid;
-	char *args[] = {NULL, NULL};
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+        pid_t pid;
+        char *args[] = {NULL, NULL};
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-{
-	printf("#cisfun$ ");
-	fflush(stdout);
-}
+        while (1)
+        {
+                if (isatty(STDIN_FILENO))
+                {
+                        printf("#cisfun$ ");
+                        fflush(stdout);
+                }
 
-		read = getline(&line, &len, stdin);
+                read = getline(&line, &len, stdin);
 
-		if (read == -1)
-		{
-			printf("\n");
-			free(line);
-			exit(0);
-		}
+                if (read == -1)
+                {
+                        free(line);
+                        exit(0);
+                }
 
-		line[read - 1] = '\0';
-		args[0] = line;
+                if (read > 0 && line[read - 1] == '\n')
+                        line[read - 1] = '\0';
 
-		pid = fork();
+                if (is_empty(line))
+                        continue;
 
-		if (pid == 0)
-		{
-			execve(args[0], args, NULL);
-			perror("./shell");
-			exit(1);
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
+                args[0] = line;
 
-	return (0);
+                pid = fork();
+
+                if (pid == 0)
+                {
+                        if (execve(args[0], args, NULL) == -1)
+                        {
+                                perror("./hsh");
+                                exit(1);
+                        }
+                }
+                else
+                {
+                        wait(NULL);
+                }
+        }
+
+        free(line);
+        return (0);
 }
