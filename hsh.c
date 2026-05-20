bcd467065cd46d8f8dfd1268597295788bf28295
Lama <lama72316@gmail.com>
2026-05-20 15:51:27 +0000

    5 Fix simple shell 0.1

--- hsh.c
+++ hsh.c
@@ -1,96 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
-/**
- * is_empty - checks if string is only spaces
- */
 int is_empty(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i])
-        {
-                if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-                        return (0);
-                i++;
-        }
-        return (1);
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-/**
- * trim_newline - remove \n
- */
 void trim_newline(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i])
-        {
-                if (str[i] == '\n')
-                {
-                        str[i] = '\0';
-                        return;
-                }
-                i++;
-        }
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			str[i] = '\0';
+			return;
+		}
+		i++;
+	}
 }
 
-/**
- * main - simple shell
- */
+char *trim_spaces(char *str)
+{
+	int end;
+
+	while (*str == ' ' || *str == '\t')
+		str++;
+
+	end = strlen(str) - 1;
+	while (end > 0 && (str[end] == ' ' || str[end] == '\t'))
+	{
+		str[end] = '\0';
+		end--;
+	}
+	return (str);
+}
+
 int main(void)
 {
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t read;
-        pid_t pid;
+	char *line = NULL;
+	char *cmd;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
 
-        while (1)
-        {
-                if (isatty(STDIN_FILENO))
-                {
-                        printf("#cisfun$ ");
-                        fflush(stdout);
-                }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("#cisfun$ ");
+			fflush(stdout);
+		}
 
-                read = getline(&line, &len, stdin);
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			exit(0);
+		}
 
-                if (read == -1)
-                {
-                        free(line);
-                        exit(0);
-                }
+		trim_newline(line);
 
-                trim_newline(line);
+		if (is_empty(line))
+			continue;
 
-                if (is_empty(line))
-                        continue;
+		cmd = trim_spaces(line);
 
-                pid = fork();
+		pid = fork();
+		if (pid == 0)
+		{
+			char *argv[2];
 
-                if (pid == 0)
-                {
-                        char *argv[2];
+			argv[0] = cmd;
+			argv[1] = NULL;
+			if (execve(cmd, argv, environ) == -1)
+			{
+				perror("./hsh");
+				exit(1);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
 
-                        argv[0] = line;
-                        argv[1] = NULL;
-
-                        if (execve(argv[0], argv, environ) == -1)
-                        {
-                                perror("./hsh");
-                                exit(1);
-                        }
-                }
-                else
-                {
-                        wait(NULL);
-                }
-        }
-
-        free(line);
-        return (0);
+	free(line);
+	return (0);
 }
